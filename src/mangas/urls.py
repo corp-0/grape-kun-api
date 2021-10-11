cce8d8bcd807cb2802009024c76f397bb5322de3
Andrés Riquelme <andresfranco.rs@gmail.com>
2021-10-11 16:07:52 -0300

    feat: nueva vista de detalle para mangas

--- src/mangas/urls.py
+++ src/mangas/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import MangaPreviewView
+from .views import MangaPreviewView, MangaDetalleView
 
 app_name = "mangas"
 
 urlpatterns = [
-   path("catalogo", MangaPreviewView.as_view(), name="catalogo")
+   path("catalogo/", MangaPreviewView.as_view(), name="catalogo"),
+   path("catalogo/<id>/", MangaDetalleView.as_view(), name="detalle")
 ]
--- src/mangas/views.py
+++ src/mangas/views.py
@@ -1,7 +1,36 @@
 from rest_framework.generics import ListAPIView
 from .models import MangaPreview
 from .serializers import MangaPreviewSerializer
+from rest_framework import status
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from manga_scrap.modelos import MangaPreview as Preview, Genero
+from manga_scrap.proveedores.mangaList import MangaList
+import json
 
 class MangaPreviewView(ListAPIView):
     queryset = MangaPreview.objects
     serializer_class = MangaPreviewSerializer
+
+class MangaDetalleView(APIView):
+    def get(self, request, id=None):
+        manga_list = MangaList()
+
+        if id is None:
+            return Response({"Error": "ID de consulta inválida"}, status=status.HTTP_400_BAD_REQUEST)
+        try:
+            datos_preview = MangaPreview.objects.get(id=id)
+        except MangaPreview.DoesNotExist:
+            return Response(status=status.HTTP_404_NOT_FOUND)
+
+        generos = [Genero(g) for g in datos_preview.generos_como_lista]
+
+        preview = Preview(
+            nombre=datos_preview.nombre,
+            enlace_imagen=datos_preview.enlace_img,
+            enlace_manga=datos_preview.enlace_manga,
+            generos=generos
+        )
+
+        respuesta = json.loads(manga_list.construir_manga(preview).to_json_string())
+        return Response(respuesta, status=status.HTTP_200_OK)
