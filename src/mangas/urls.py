0d43e65929568aadc73a4c5df137c38ba0b0a430
Andrés Riquelme <andresfranco.rs@gmail.com>
2021-10-31 11:24:32 -0300

    feat: agregada vista de imágenes para un capítulo

--- src/mangas/urls.py
+++ src/mangas/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import MangaPreviewView, MangaDetalleView
+from .views import MangaPreviewView, MangaDetalleView, CapituloDetalleView
 
 app_name = "mangas"
 
 urlpatterns = [
    path("catalogo/", MangaPreviewView.as_view(), name="catalogo"),
-   path("catalogo/<id>/", MangaDetalleView.as_view(), name="detalle")
+   path("catalogo/<int:id>/", MangaDetalleView.as_view(), name="detalle"),
+   path("catalogo/<int:id>/<int:capitulo>", CapituloDetalleView.as_view(), name="capitulo"),
 ]
--- src/mangas/views.py
+++ src/mangas/views.py
@@ -9,11 +9,19 @@ from manga_scrap.proveedores.mangaList import MangaList
 import json
 
 class MangaPreviewView(ListAPIView):
+    """
+    Vista para obtener una lista de mangas
+    """
     queryset = MangaPreview.objects.all()
     serializer_class = MangaPreviewSerializer
 
 class MangaDetalleView(APIView):
-    def get(self, request, id=None):
+    """
+    Vista para obtener el detalle de un manga.
+    :param: id: id del manga
+    :return: json con el detalle del manga
+    """
+    def get(self, request, id: int):
         manga_list = MangaList()
 
         if id is None:
@@ -35,3 +43,39 @@ class MangaDetalleView(APIView):
 
         respuesta = json.loads(manga_list.obtener_manga_detalle(preview).to_json_string())
         return Response(respuesta, status=status.HTTP_200_OK)
+
+class CapituloDetalleView(APIView):
+    """
+    Vista para obtener los datos de un capítulo.
+    :param: id: id del capítulo
+    :param: capitulo: índice del capítulo que pertenece a la vista de detalle.
+    :return: json con los datos del capítulo, incluyendo sus imágenes
+    """
+    def get(self, request, id: int, capitulo: int):
+        manga_list = MangaList()
+
+        if id is None:
+            return Response({"Error": "ID de consulta inválida"}, status=status.HTTP_400_BAD_REQUEST)
+        try:
+            datos_preview = MangaPreview.objects.get(id=id)
+        except MangaPreview.DoesNotExist:
+            return Response(status=status.HTTP_404_NOT_FOUND)
+        generos = [Genero(g) for g in datos_preview.generos_como_lista]
+
+        preview = Preview(
+            nombre=datos_preview.nombre,
+            enlace_imagen=datos_preview.enlace_img,
+            enlace_manga=datos_preview.enlace_manga,
+            generos=generos,
+            contenido_adulto=datos_preview.contenido_adulto
+        )
+        detalle = manga_list.obtener_manga_detalle(preview)
+        try:
+            capitulo = detalle.capitulos[capitulo]
+        except IndexError:
+            return Response(status=status.HTTP_404_NOT_FOUND)
+
+        respuesta = json.loads(manga_list.obtener_capitulo_detalle(capitulo.enlace).to_json_string())
+        return Response(respuesta, status=status.HTTP_200_OK)
+
+
